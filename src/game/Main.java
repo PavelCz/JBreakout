94d43469a8db43507421d8f07511d588d97af139
pavel.czempin <pavel.czempin@w4029264.ca0375.muenchen.musin.de>
2013-11-29 09:25:41 +0100

    MyVector2f now has setAngleDegrees and setAngleRadians

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,17 +1,17 @@
 package game;
 
-
 public class Main {
 
 	public static void main(String[] argv) {
-		//Game game = new Game();
-		//game.init();
-		//game.start();
-		
-		MyVector2f m = new MyVector2f(10, -10);
-		m.rotateDegrees(-10);
+		MyVector2f m = new MyVector2f(10, 10);
+		m.setAngleDegrees(55);
 		System.out.println(m.getLength());
 		System.out.println(m.getAngleDegrees());
 		System.out.println(m.getAngleRadians());
-	} 
+
+		Game game = new Game();
+		game.init();
+		game.start();
+
+	}
 }
