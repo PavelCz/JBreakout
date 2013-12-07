f49111a5e08646c3899eb733574e87a5ca7ee262
Valep42 <p.czempin@gmx.de>
2013-12-07 12:53:38 +0100

    ---

--- src/game/Racket.java
+++ src/game/Racket.java
@@ -4,42 +4,39 @@ import org.lwjgl.util.vector.Vector2f;
 
 public class Racket extends Entity {
 
-    
-    private int width;
-    private int height;
+	private int width;
+	private int height;
 
-    private float speed;
+	private float speed;
 
-    public Racket(Window gameWindow) {
-	super(gameWindow);
-	
+	public Racket(Window gameWindow) {
+		super(gameWindow);
 
-	this.speed = 0.75f;
-	this.width = 100;
-	this.height = 25;
-	this.coordinates = new Vector2f((this.gameWindow.getWidth() - this.width) / 2, this.gameWindow.getHeight() - this.height
-		- 50);
-	
-	this.renderObject = new Rectangle(coordinates.getX(), coordinates.getY(), width, height);
-	this.renderObject.setColor(0f, 0f, 1f);
+		this.speed = 0.75f;
+		this.width = 100;
+		this.height = 25;
+		this.coordinates = new Vector2f(
+				(this.gameWindow.getWidth() - this.width) / 2,
+				this.gameWindow.getHeight() - this.height - 50);
 
-    }
+		this.renderObject = new Rectangle(coordinates.getX(),
+				coordinates.getY(), width, height);
+		this.renderObject.setColor(0f, 0f, 1f);
 
-   
-
-    
-
-    public void moveHorizontally(int delta, float x) {
-	this.coordinates.setX(this.coordinates.getX() + speed * x * delta);
-	if (this.coordinates.getX() < 0) {
-	    this.coordinates.setX(0);
-	} else if (this.coordinates.getX() + this.width > this.gameWindow.getWidth()) {
-	    this.coordinates.setX(this.gameWindow.getWidth() - this.width);
 	}
-    }
-    
-    public int getWidth() {
-	return this.width;
-    }
+
+	public void moveHorizontally(int delta, float x) {
+		this.coordinates.setX(this.coordinates.getX() + speed * x * delta);
+		if (this.coordinates.getX() < 0) {
+			this.coordinates.setX(0);
+		} else if (this.coordinates.getX() + this.width > this.gameWindow
+				.getWidth()) {
+			this.coordinates.setX(this.gameWindow.getWidth() - this.width);
+		}
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
 
 }
